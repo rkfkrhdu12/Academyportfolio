e980aa1b6fcb1f390a60f97e40af39a186159262
yuchanahn <37448462+yuchanahn@users.noreply.github.com>
2018-11-09 14:27:28 +0900

    ll
    l

---
+++ 클라/Client/Assets/EquipSlot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipSlot : MonoBehaviour
+{
+    List<ItemSlot> mslot = new List<ItemSlot>();
+
+    private void Start()
+    {
+        foreach (var Slot in GetComponentsInChildren<ItemSlot>())
+        {
+            mslot.Add(Slot);
+        }
+    }
+    
+    public void Add(PlayerSystem Item, int SlotNum)
+    {
+        mslot[SlotNum].Item = Item;
+    }
+
+}
